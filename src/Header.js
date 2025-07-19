058e84a7a1b7f67aee2f418490343a0859335774
Sujit Tripathy <Sujit.Tripathy@walmart.com>
2025-07-19 14:31:55 -0700

    Refactor: Adjust JSON Formatter tree view background and header icon placement

--- src/Header.js
+++ src/Header.js
@@ -2,23 +2,12 @@ import React from 'react';
 import { Navbar, Container, Form } from 'react-bootstrap';
 
 const Header = ({ theme, toggleTheme, wrapTextEnabled, toggleWrapText }) => {
-  const navbarVariant = theme === 'dark' ? 'dark' : 'light';
-  const navbarBg = theme === 'dark' ? 'dark' : 'light';
-
   return (
     <>
       <Navbar expand="lg" fixed="top" className="custom-header">
         <Container>
           <Navbar.Brand href="#" style={{ fontWeight: 'bold', fontSize: '1.5rem' }}>{`{ JSON Tools }`}</Navbar.Brand>
-          <Form className="d-flex align-items-center">
-            <Form.Check 
-              type="switch"
-              id="wrap-text-switch"
-              label="Wrap Text"
-              checked={wrapTextEnabled}
-              onChange={toggleWrapText}
-              className="me-3"
-            />
+          <Form className="d-flex align-items-center" style={{ marginLeft: 'auto' }}>
             <Form.Check 
               type="switch"
               id="theme-switch"
